41ee5e08686e5acbfa09b5825de86a1b9cdab523
Lubova Tetrjakova <l.tetrjakova@intrum.com>
2019-04-03 18:31:40 +0300

    Fixed the RegEx Expression

--- UFirstTechTaskByLu/Services/Serializer.cs
+++ UFirstTechTaskByLu/Services/Serializer.cs
@@ -8,14 +8,7 @@ namespace UFirstTechTaskByLu.Services
     {
         public static ParsedLog Deserialize(string line)
         {
-            var template = "{0} [{1}:{2}:{3}:{4}] \"{5} {6} {7}/{8}\" {9} {10}";
-            //141.243.1.172 [29:23:53:25] "GET /Software.html HTTP/1.0" 200 1497
-
-            //Handels regex special characters.
-            template = Regex.Replace(template, @"[\\\^\$\.\|\?\*\+\(\)\[\]]", v => "\\"
-                 + v.Value);
-
-            var pattern = "^" + Regex.Replace(template, @"\{[0-9]+\}", "(.*?)") + "$";
+            var pattern = "^(.*)\\s+\\[(.*):(.*):(.*):(.*)\\]\\s+\"(.*)\\s+(.*)\\s+(....)/?(.*?)\"\\s+(...)\\s*(.*)$";
 
             var regex = new Regex(pattern);
             var match = regex.Match(line);
