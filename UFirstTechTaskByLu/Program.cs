11260990f24dc2c3c5057f82390ad15dfc161e93
Lubova Tetrjakova <lubova.tretjakova@gmail.com>
2019-04-04 11:43:12 +0300

    Fixed the program workflow

--- UFirstTechTaskByLu/Program.cs
+++ UFirstTechTaskByLu/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UFirstTechTaskByLu.Models;
 using UFirstTechTaskByLu.Services;
 
@@ -11,9 +12,30 @@ namespace UFirstTechTaskByLu
     {
         private static void Main(string[] args)
         {
+            List<string> argumentsList = args.OfType<string>().ToList();
+
             var parsedLogs = new List<ParsedLog>();
 
-            var lines = File.ReadAllLines(@"../../Resources/epa-http.txt");
+            Console.WriteLine("Please enter the input file location");
+            string inputFileLocation = Console.ReadLine();
+            
+            while (!Directory.Exists(inputFileLocation))
+            {
+                Console.WriteLine("The INPUT location is not valid or not found, please enter the valid existing INPUT file location");
+                inputFileLocation = Console.ReadLine();
+            }
+
+            var lines = File.ReadAllLines(inputFileLocation + "\\input.txt");
+
+            Console.WriteLine("Please enter the output file location");
+            string outputFileLocation = Console.ReadLine();
+
+            while (!Directory.Exists(outputFileLocation))
+            {
+                Console.WriteLine("The OUTPUT location is not valid or not found, please enter the valid existing OUTPUT file location");
+                outputFileLocation = Console.ReadLine();
+            }
+
             foreach (string line in lines)
             {
                 parsedLogs.Add(Serializer.Deserialize(line));
@@ -21,9 +43,9 @@ namespace UFirstTechTaskByLu
 
             var jsonString = JsonConvert.SerializeObject(parsedLogs, Formatting.Indented);
 
-            File.WriteAllText("jsonParsed.json", jsonString);
+            File.WriteAllText(Path.Combine(outputFileLocation, "output.json"), jsonString);
 
-            Console.WriteLine("There were {0} lines.", parsedLogs.Count);
+            Console.WriteLine("{0} lines were parsed and added into JSON-Array in the following location: {1}\\output.json", parsedLogs.Count, outputFileLocation);
             Console.ReadLine();
         }
     }
